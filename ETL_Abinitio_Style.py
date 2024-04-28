8f8bcc9a345a5771ab667f85655b0594bf27c1e2
aguslsan <44140618+aguslsan@users.noreply.github.com>
2024-04-28 18:06:06 -0300

    Add files via upload

--- ETL_Abinitio_Style.py
+++ ETL_Abinitio_Style.py
@@ -5,6 +5,7 @@ import ETL_Abinitio_Stile_COMMON as COMMON
 import yaml
 import ETL_JOINS
 
+
 def leer_archivo(archivo_path, campos, delimiter):
     with open(archivo_path, "r", encoding="UTF-8") as archivo:
         reader = csv.reader(archivo, delimiter=delimiter)
@@ -19,6 +20,7 @@ def leer_archivo(archivo_path, campos, delimiter):
 
     return lista_input
 
+
 def load_config(transformations_file):
     with open(transformations_file, 'r') as f:
         transformations = yaml.safe_load(f)
@@ -28,6 +30,7 @@ def load_config(transformations_file):
 def read_files(transformations):
     for x in transformations["files"]:
         globals()[x["variable_name"]] = leer_archivo(x["file_name"], x["schema"].split("|"), "|")
+
 
 # ________________________________________________________
 
@@ -55,7 +58,35 @@ def escribir_archivo(lista, path, delimitador):
 
 # ________________________________________________________
 
+def execute_filters(transformations):
+    for filter in transformations["filters"]:
+        """
+        # globals()[filter["variable_name"]] =
+        # print(globals()[filter["variable_name"]])
+        contador=0
+        for registro in globals()[filter["variable_name"]]:
+            if not eval(filter["code"]):
+                print("true")
+                # del globals()[filter["variable_name"]][contador]
+                globals()[filter["variable_name"]].remove(registro)
+            print("false")
+            contador = contador + 1
+        """
+        print(type(globals()[filter["variable_name"]]))
+        print(globals()[filter["variable_name"]])
+        globals()[filter["variable_name"]] = list(filter(lambda x: eval(filter["code"]), globals()[filter["variable_name"]]))
+        print(globals()[filter["variable_name"]])
+
+
+def execute_joins(transformations):
+    for join in transformations["joins"]:
+        globals()["main"] = left_join(globals()[join["input_main"]], globals()[join["input_lookup"]],
+                                      join["key_main"], join["key_lookup"], join["prefix"])
+
+
 def execute_transformations(transformations):
+    execute_filters(transformations)
+    execute_joins(transformations)
     output_list = []
     for registro in globals()["main"]:
         output_list.append(transform(registro, transformations))
@@ -79,18 +110,65 @@ def transform(registro, transformations):
 
     return output_record
 
+
 # __________________________________________________________
 
+def renombrar_claves(diccionario, prefijo):
+    diccionario_renombrado = {prefijo + antigua_clave: valor for antigua_clave, valor in diccionario.items()}
+    return diccionario_renombrado
+
+
+def left_join(list1, list2, list1_key, list2_key, prefix):
+    result = []
+    for item1 in list1:
+        joined_items = item1.copy()
+        key1 = item1.get(list1_key)
+        match_found = False
+        item2 = {}
+
+        for item2 in list2:
+            key2 = item2.get(list2_key)
+
+            if key1 == key2:
+                joined_items.update(renombrar_claves(item2.copy(), prefix))
+                match_found = True
+                break  # Salir del bucle una vez que se encuentra una coincidencia
+
+        if not match_found:
+            # Si no hay coincidencia, agregar campos vacíos del diccionario con prefijo
+            empty_dict = {f"{prefix}{k}": "" for k in item2.keys()}
+            joined_items.update(empty_dict)
+
+        result.append(joined_items)
+
+    return result
+
+
+def inner_join(list1, list2, list1_key, list2_key, prefix):
+    result = []
+    for item1 in list1:
+        key1 = item1.get(list1_key)
+
+        for item2 in list2:
+            key2 = item2.get(list2_key)
+
+            if key1 == key2:
+                joined_items = item1.copy()
+                joined_items.update(renombrar_claves(item2.copy(), prefix))
+                result.append(joined_items)
+
+    return result
+
+
+# __________________________________________________________
 
 mappings = load_config("mappings.yaml")
 
 read_files(mappings)
 
-
-
 lista_output = execute_transformations(mappings)
 
-for x in lista_output:
-    print(x)
+#for x in lista_output:
+ #   print(x)
 
 escribir_archivo(lista=lista_output, path="out.csv", delimitador="|")
